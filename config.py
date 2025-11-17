624f57851ccb1682ea48b4928127c01516b11d6a
ROHIT12-bit <reddyrohit823@gmail.com>
2025-11-17 13:19:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ class Config(object):
     ADMIN = [int(admin) if id_pattern.search(
         admin) else admin for admin in os.environ.get('ADMIN', '8367080346').split()]  # ⚠️ Required
     
-    FORCE_SUB = os.environ.get("FORCE_SUB", "-1003322423783") # ⚠️ Required Username without @
+    FORCE_SUB = os.environ.get("FORCE_SUB", "JAIBABUVARANSI") # ⚠️ Required Username without @
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003448988619"))  # ⚠️ Required
     FLOOD = int(os.environ.get("FLOOD", '10'))
     BANNED_USERS = set(int(x) for x in os.environ.get(
